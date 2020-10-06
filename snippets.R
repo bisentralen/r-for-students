a921cd929ad1e24fc6238c2c56c8339adb2534b6
Fredrik H. Juell <fjuell@users.noreply.github.com>
2020-10-06 17:18:59 +0200

    Add files via upload

--- snippets.R
+++ snippets.R
@@ -1,6 +1,5 @@
 # R script for testing simple stuff
 
-
 # Defining three numerical vectors
 a <- c(1, 3.4, 2.3, 5.1)
 b <- c(0.1, 13.4, 12.3, 5.1)
@@ -19,4 +18,6 @@ sd(a)
 weighted.mean(a, w)
 
 
+# Defining a vector c, as a and b put together
+c <- append(a, b)
 
