9553c6351345ca18c6846ffd1e66b3369a020ba9
Fredrik H. Juell <fjuell@users.noreply.github.com>
2020-10-06 16:56:38 +0200

    Add files via upload

--- snippets.R
+++ snippets.R
@@ -1,14 +1,22 @@
+# R script for testing simple stuff
+
+
+# Defining three numerical vectors
 a <- c(1, 3.4, 2.3, 5.1)
 b <- c(0.1, 13.4, 12.3, 5.1)
 w <- c(1, 1, 2, 1)
 
-# Standard deviation of values in a
-sd(a)
 
 # Mean of values in a
 mean(a)
 
+
+# Standard deviation of values in a
+sd(a)
+
+
 # Weighted mean of a, using weights in w
 weighted.mean(a, w)
+
 
 
